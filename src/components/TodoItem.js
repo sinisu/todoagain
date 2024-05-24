1ff8faa0be188f197d1516cc1c147333c84417f6
yisoo song <yisoo1049@gmail.com>
2024-05-25 05:53:59 +0900

    second commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const TodoItem = () => {
+const TodoItem = ({item,deleteItem}) => {
+
   return (
     <Row>
       <Col xs={12}>
         <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+          <div className="todo-content">{item.task}</div>
 
           <div>
-            <button className="button-delete">삭제</button>
+            <button className="button-delete" onClick={()=>deleteItem(item._id)}>삭제</button>
             <button className="button-delete">끝남</button>
           </div>
         </div>
